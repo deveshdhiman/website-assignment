900f93db7c2e5afc303f8ff18ee4818e05d6026b
devesh <devesh.kumar.dhiman@sap.com>
2022-12-12 13:48:14 +0530

    Added: website basic 1st cut

---
+++ script.js
@@ -0,0 +1,21 @@
+let http = new XMLHttpRequest(); // creating xmlhttp-request object
+http.open('get', 'items.json', true); // preparing request with open object
+http.send(); // sending the request
+
+http.onload = function(){
+    if(this.readyState == 4 && this,this.status == 200){
+        let items = JSON.parse(this.responseText);
+        let output = "";
+        for(let item of items){
+            output += `
+            <div class="feature-col">
+                <h3>${item.name}</h3>
+                <br>
+                <a href=${item.eula_url}>click here to know more</a>
+            </div>
+
+            `;
+        }
+        document.querySelector(".products").innerHTML = output;
+    }
+}
